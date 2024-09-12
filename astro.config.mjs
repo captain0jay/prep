c260b418d1165fd82322b4cac1d0f304d97a7fa2
captain0jay <captainstorage183@gmail.com>
2024-09-12 16:56:52 +0530

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,42 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://data.github.io',
+  	base: 'docs',
+	integrations: [
+		starlight({
+			title: 'Captain Jay',
+			social: {
+				github: 'https://github.com/captain0jay',
+				twitter: 'https://twitter.com/captain0jay',
+				linkedin: 'https://www.linkedin.com/in/captain0jay',
+			},
+			sidebar: [
+				{
+					label: 'DSA',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'DSA stuff', slug: 'dsa/example' },
+					],
+				},
+				{
+					label: 'Algos',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'New algorithms', slug: 'algos/example' },
+					],
+				},
+				{
+					label: 'SQL',
+					autogenerate: { directory: 'SQL' },
+				},
+			],
+			customCss: [
+			'./src/styles/theme.css'
+			],
+	}),
+	],
+});
